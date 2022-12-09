7e01d67516b3079dc2d179db64d2173f8d155931
Subramanyarao11 <subramanya11rao@gmail.com>
2022-12-09 12:33:40 +0530

    Added 3 layouts

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
-<div class="bg-black h-screen">
-
-</div>
+  <div class="h-full">
+    <Home />
+    <!-- <login /> -->
+    <!-- <Forgot /> -->
+  </div>
 </template>
 
 <script>
-
+import Home from "./Home.vue";
+// import Login from './Login.vue'
+// import Forgot from './Forgot.vue'
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    Home,
+    // Login,
+    // Forgot,
+  },
+};
 </script>
 
 <style>
-
 </style>
